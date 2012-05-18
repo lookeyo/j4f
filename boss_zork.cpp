90224ccc9bb3bd5b579c213ab89e029585f08022
lookeyo <lookeyo@web.de>
2012-05-18 17:56:39 +0200

    adding zork.cpp

---
+++ boss_zork.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2012 Zork
+ */
+
+#include "ScriptPCH.h"
+
+enum Spells
+{
+    SPELL_STRIKE				= 109211,
+    SPELL_FRENZY				= 81173,
+};
+
+enum Events
+{
+    EVENT_NONE,
+    EVENT_STRIKE,
+    EVENT_FRENZY,
+};
+
+class boss_zork : public CreatureScript
+{
+public:
+    boss_zork() : CreatureScript("boss_zork") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new boss_zorkAI (creature);
+    }
+
+    struct boss_zorkAI : public BossAI
+    {
+
+        bool enraged;
+
+        void EnterCombat(Unit* /*who*/)
+        {
+            _EnterCombat();
+            enraged = false;
+            events.ScheduleEvent(EVENT_FRENZY, 16000);
+            events.ScheduleEvent(EVENT_STRIKE, 20000);
+
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim() || !CheckInRoom())
+                return;
+
+            events.Update(diff);
+
+            while (uint32 eventId = events.ExecuteEvent())
+            {
+                switch (eventId)
+                {
+                    case EVENT_FRENZY:
+						DoCast(me, SPELL_FRENZY), true);
+                        events.ScheduleEvent(EVENT_FRENZY, 16000);
+                        break;
+                    case EVENT_STRIKE:
+						DoCastVictim(SPELL_STRIKE);
+                        events.ScheduleEvent(EVENT_STRIKE, 20000);
+                        break;
+                }
+            }
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
+void AddSC_boss_zork()
+{
+    new boss_zork();
+}
